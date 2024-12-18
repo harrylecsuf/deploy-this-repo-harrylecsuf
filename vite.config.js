06fafc70181b350313416d1919dda613a642c7a9
Harry Le <harrylrc@csu.fullerton.edu>
2024-12-18 15:44:16 -0800

    Fix server binding to PORT for Heroku

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-	plugins: [
-		sveltekit(),
-		...(mode === 'development' ? [mkcert()] : []) // Include mkcert only in development
-	],
-	server: {
-		https: mode === 'development', // Enable HTTPS only in development
-		proxy: {}, // Essential to avoid "can't use Symbol where you need a string" error
-	},
+    plugins: [
+        sveltekit(),
+        ...(mode === 'development' ? [mkcert()] : [])
+    ],
+    server: {
+        host: true, // Listen on all available network interfaces
+        port: process.env.PORT || 3000, // Use PORT from the environment or default to 3000
+        proxy: {} // Avoid "can't use Symbol where you need a string" error
+    }
 }));
