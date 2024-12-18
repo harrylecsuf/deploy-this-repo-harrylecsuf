ec95d1a682be3f3ade707d3a66314578349b82be
Harry Le <harrylrc@csu.fullerton.edu>
2024-12-18 15:25:44 -0800

    Conditionally include vite-plugin-mkcert for development

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import mkcert from "vite-plugin-mkcert"
+import mkcert from "vite-plugin-mkcert";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-
-export default defineConfig({
-	plugins: [sveltekit(),
-		mkcert()
+export default defineConfig(({ mode }) => ({
+	plugins: [
+		sveltekit(),
+		...(mode === 'development' ? [mkcert()] : []) // Include mkcert only in development
 	],
 	server: {
-		https: true,
-		proxy: {}, // essential to avoid "can't use Symbol where you need a string" error
+		https: mode === 'development', // Enable HTTPS only in development
+		proxy: {}, // Essential to avoid "can't use Symbol where you need a string" error
 	},
-})
+}));
